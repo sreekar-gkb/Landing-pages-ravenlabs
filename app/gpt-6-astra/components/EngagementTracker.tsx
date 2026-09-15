9489a0a80ab7c1f33664337e3baef57383a8503b
sreekar-gkb <sreekar@gkblabs.com>
2026-09-15 08:44:19 +0530

    Delete app/gpt-6-astra directory

--- app/gpt-6-astra/components/EngagementTracker.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useEffect } from 'react'
-import { initEngagementTracking } from '@/lib/tracking'
-
-export default function EngagementTracker({ campaign }: { campaign: string }) {
-  useEffect(() => initEngagementTracking(campaign), [campaign])
-  return null
-}
